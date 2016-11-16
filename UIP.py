9d8e84281ebbc47fd59173d81d3f9a6483ffd479
vishva_nath <vishvanath45@github.com>
2016-11-16 19:41:06 +0530

    Added a nice message For UIP to show on the main screen

--- UIP.py
+++ UIP.py
@@ -1,6 +1,12 @@
 from scheduler import scheduler
 
+print ("Hey this is UIP! you can use it to download images from reddit and also to schedule the setting of these images as your desktop wallpaper.")
+
 if __name__ == "__main__":
+<<<<<<< 2185a6f5ba43b272c70dd78f1edf0696934debb4
   print("Press 1 to connect to internet or any other key to use this application in offline mode")
   offline = int(input()) != 1
   scheduler(offline)
+=======
+	scheduler()
+>>>>>>> Added a nice message For UIP to show on the main screen
